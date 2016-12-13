1f8e28ed44fb083831f9b3eaad5c71907c8621c3
Max LIU <mrliucan@foxmail.com>
2016-12-13 15:45:54 +1100

    one way and two way binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id : number;
+  name : string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  // template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './template/mainpage.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  hero: Hero = {
+    id: 1,
+    name: 'Max_LIU'
+  };
+}
