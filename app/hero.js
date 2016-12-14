5e4950446af48f3606affe8b23222c7de69c0316
Max LIU <mrliucan@foxmail.com>
2016-12-14 14:31:54 +1100

    Add js files

---
+++ app/hero.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 70243 on 2016/12/14.
+ */
+"use strict";
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
